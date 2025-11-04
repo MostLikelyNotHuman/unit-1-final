547e5675ac306723212337152719be1acb4234ac
Danny H <dannypercival14@gmail.com>
2025-11-03 20:53:09 -0500

    basic navigation

--- final-project/src/App.jsx
+++ final-project/src/App.jsx
@@ -1,15 +1,24 @@
+import { Route, Routes } from 'react-router';
 import './App.css'
 import HelpPage from './components/HelpPage';
 import HomePage from './components/HomePage';
+import IntervalPractice from './components/IntervalPractice';
 import NotePractice from './components/NotePractice';
+import RhythmPractice from './components/RhythmPractice';
 
 function App() {
 
   return (
     <>
-      <HelpPage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/practice-notes" element={<NotePractice />} />
+        <Route path="/practice-intervals" element={<IntervalPractice />} />
+        <Route path="/practice-rhythm" element={<RhythmPractice />} />
+        <Route path="/help" element={<HelpPage />} />
+      </Routes>
     </>
   );
-}
+};
 
-export default App
+export default App;
--- final-project/src/components/pieces/Header.jsx
+++ final-project/src/components/pieces/Header.jsx
@@ -1,14 +1,15 @@
+import { Link } from "react-router";
 import Button from "./Button";
 
 const Header = () => {
 
     return (
         <header>
-            <h2>LOGO</h2>
-            <Button text={'Notes'}/>
-            <Button text={'Intervals'}/>
-            <Button text={'Rhythm'}/>
-            <Button text={'Help'}/>
+            <h2><Link to="/">LOGO</Link></h2> 
+            <p><Link to="/practice-notes">Notes</Link></p>
+            <p><Link to="/practice-intervals">Intervals</Link></p>
+            <p><Link to="/practice-rhythm">Rhythm</Link></p>
+            <p><Link to="/help">Help</Link></p>
         </header>
     );
 }
