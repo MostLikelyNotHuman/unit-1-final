aacecd8b37c29e86bf367c1f8024bd6528e8c0fe
Danny H <dannypercival14@gmail.com>
2025-11-05 20:12:10 -0500

    added notes and rhythms data, placeholder immages

---
+++ final-project/src/assets/notes.js
@@ -0,0 +1,50 @@
+export const notes = {
+    c: {
+        pitch: 0,
+        text: 'C',
+        img: './notes/PLACEHOLDER/c.png',
+        audio: ''
+    },
+    d: {
+        pitch: 2,
+        text: 'D',
+        img: './notes/PLACEHOLDER/d.png',
+        audio: ''
+    },
+    e: {
+        pitch: 4,
+        text: 'E',
+        img: './notes/PLACEHOLDER/e.png',
+        audio: ''
+    },
+    f: {
+        pitch: 5,
+        text: 'F',
+        img: './images/notes/PLACEHOLDER/f.png',
+        audio: ''
+    },
+    g: {
+        pitch: 7,
+        text: 'G',
+        img: './images/notes/PLACEHOLDER/g.png',
+        audio: ''
+    },
+    a: {
+        pitch: 9,
+        text: 'A',
+        img: './images/notes/PLACEHOLDER/a.png',
+        audio: ''
+    },
+    b: {
+        pitch: 11,
+        text: 'B',
+        img: './images/notes/PLACEHOLDER/b.png',
+        audio: ''
+    },
+    c2: {
+        pitch: 12,
+        text: 'C',
+        img: './images/notes/PLACEHOLDER/c2.png',
+        audio: ''
+    }
+};
---
+++ final-project/src/assets/rhythms.js
@@ -0,0 +1,26 @@
+export const rhythms = {
+    wholeNote: {
+        duration: 4,
+        text: 'Whole Note',
+        img: './images/rhythm/PLACEHOLDER/whole-note.png',
+        audio: ''
+    },
+    halfNote: {
+        duration: 2,
+        text: 'Half Note',
+        img: './images/rhythm/PLACEHOLDER/half-note.png',
+        audio: ''
+    },
+    quarterNote: {
+        duration: 1,
+        text: 'Quarter Note',
+        img: './images/rhythm/PLACEHOLDER/quarter-note.png',
+        audio: ''
+    },
+    eighthNotes: {
+        duration: 1,
+        text: 'Two Eighth Notes',
+        img: './images/rhythm/PLACEHOLDER/eighth-notes.png',
+        audio: ''
+    },
+};
