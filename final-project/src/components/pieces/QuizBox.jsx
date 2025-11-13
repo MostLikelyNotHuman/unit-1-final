093d84a9bb242eedafec4a1e183f98c72e99f136
NotHuman <dannypercival14@gmail.com>
2025-11-12 19:41:28 -0600

    small changes

--- final-project/src/components/pieces/QuizBox.jsx
+++ final-project/src/components/pieces/QuizBox.jsx
@@ -2,14 +2,18 @@ import Button from "./Button";
 import HelpButton from "./HelpButton";
 import ToggleSwitch from "./ToggleSwitch";
 
-const QuizBox = ({ questionText, questionContent, answer1, answer2, answer3, answer4 }) => {
+const QuizBox = ({ questionText, questionImage, answer1, answer2, answer3, answer4 }) => {
 
     return(
         <div id='quizBox'>
             <h4>{questionText}</h4>
-            <p>{questionContent}</p>
-            <ToggleSwitch />
-            <HelpButton />
+            <div id="question-content">
+                <img src="questionImage"></img>
+                <div id="question-switches">
+                    <ToggleSwitch />
+                    <HelpButton />
+                </div>
+            </div>
             <div id="question-answers">
                 <Button text={answer1}></Button>
                 <Button text={answer2}></Button>
