2d353fe532345ced5fbf88ecf13463494acfdeed
NotHuman <dannypercival14@gmail.com>
2025-11-13 01:25:43 -0600

    help page

--- final-project/src/components/HelpPage.jsx
+++ final-project/src/components/HelpPage.jsx
@@ -30,7 +30,60 @@ const HelpPage = () => {
                     <p>The word 'interval' refers to the distance in pitch between two notes.
                     The smallest distance between two notes in western music is called a <strong>half-step</strong> or a <strong>semitone</strong>.</p>
                     <h3>Rhythm</h3>
-                    <p>**about rhythm**</p>
+                    <p>Notes placed on a staff don't just tell you their pitch, they also tell you their rhythm.
+                    All of the examples on this page have used something called a <strong>quarter note</strong> to help visualize
+                    the different concepts, but there are many more. A quarter note represents a single beat ... </p>
+                    <table>
+                        <tr>
+                            <th>Note ...</th>
+                            <th>is equal in duration to ...</th>
+                        </tr>
+                        <tr>
+                            <td>(image of whole note)</td>
+                            <td>(image of two half notes)</td>
+                        </tr>
+                        <tr>
+                            <td>One Whole Note</td>
+                            <td>Two Half Notes</td>
+                        </tr>
+                        <tr>
+                            <td>(image of half note)</td>
+                            <td>(image of two quarter notes)</td>
+                        </tr>
+                        <tr>
+                            <td>One Half Note</td>
+                            <td>Two Quarter Notes</td>
+                        </tr>
+                        <tr>
+                            <td>(image of quarter note)</td>
+                            <td>(image of two eighth notes)</td>
+                        </tr>
+                        <tr>
+                            <td>One Quarter Note</td>
+                            <td>Two Eighth Notes</td>
+                        </tr>
+                        <tr>
+                            <td>(image of eighth note)</td>
+                            <td>(image of two sixteenth notes)</td>
+                        </tr>
+                        <tr>
+                            <td>One Eighth Note</td>
+                            <td>Two Sixteenth Notes</td>
+                        </tr>
+                        <tr>
+                            <td>(image of sixteenth note)</td>
+                            <td>(image of two thirty-second notes)</td>
+                        </tr>
+                        <tr>
+                            <td>One Sixteenth Note</td>
+                            <td>Two Thirty-Second Notes</td>
+                        </tr>
+                    </table>
+                    <p>Theoretically this continues on forever, but notes with a shorter duration are uncommon.</p>
+                    <p>Any of these notes can be modified by placing a dot after it, which extends its normal duration by one half.
+                        For example, a quarter note is equal to two eighth notes, but a <strong>dotted quarter note</strong> is 
+                        equal to three eighth notes. [PLACE IMAGE]
+                    </p>
                 </div>
                 <div id="nav-bar">
                     <p>Navigation</p>
