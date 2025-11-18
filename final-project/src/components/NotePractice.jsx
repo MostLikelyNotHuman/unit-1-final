c307ccac447e17f11eb96ef516a2dbde03901e36
NotHuman <dannypercival14@gmail.com>
2025-11-17 18:55:47 -0600

    first pass, need to switch branches

--- final-project/src/components/NotePractice.jsx
+++ final-project/src/components/NotePractice.jsx
@@ -1,17 +1,42 @@
-import Footer from "./pieces/Footer";
-import Header from "./pieces/Header";
 import QuizBox from "./pieces/QuizBox";
+import { notes } from "../assets/notes";
 
 const NotePractice = () => {
+
+
+    let questionImage = '';
+    let answers = [];
+    let correctAnswer = '';
+
+    const retrieveQuestion = () => {
+        const editedNotes = [...notes];
+        const newQuestion = editedNotes[Math.floor(Math.random() * editedNotes.length)];
+        editedNotes.splice(newQuestion, 1);
+        correctAnswer = newQuestion.text;
+        answers.push(correctAnswer);
+        questionImage = newQuestion.img;
+        for (let i = 0; i < 3; i++) {
+            let incorrect = editedNotes[Math.floor(Math.random() * editedNotes.length)].text;
+            answers.push(incorrect);            
+        }
+        answers.sort(() => Math.random() - 0.5);
+    }
+
+    const checkAnswer = (selectedAnswer) => {
+        return selectedAnswer === correctAnswer;
+    }
+
+
+
     return(
         <>
             <QuizBox 
                 questionText={'Practice - Notes'} 
-                questionContent={'*place image*'}
-                answer1={'answer 1'}
-                answer2={'answer 2'}
-                answer3={'answer 3'}
-                answer4={'answer 4'}
+                questionContent={questionImage}
+                answer1={answers[0]}
+                answer2={answers[1]}
+                answer3={answers[2]}
+                answer4={answers[3]}
             ></QuizBox>
         </>
     );
